5e5cdcf3ade740b56152d0c7524f280d9ec5c2a7
Aidar Garikhanov <a1d4r@yandex.ru>
2020-09-10 18:09:45 +0300

    Store secret keys in a untracked file'

--- weather_app/settings.py
+++ weather_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import secrets
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gmagf&0a@8=212dd=rf2i1l)fgsn#l-ywg5%6mbi8tx4i9z3)0'
+SECRET_KEY = secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
