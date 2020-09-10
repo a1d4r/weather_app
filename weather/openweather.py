643d60e6d2fdcc3d0adc6a74a9c97e523b0876fb
Aidar Garikhanov <a1d4r@yandex.ru>
2020-09-10 18:41:19 +0300

    Create python module for OpenWeatherAPI

---
+++ weather/openweather.py
@@ -0,0 +1,33 @@
+import requests
+
+import secrets
+
+
+class QuotaError(Exception):
+    """Exception to be raised when the app exceeds a limit of API calls"""
+    pass
+
+
+def get_weather_data(city):
+    """
+    Return weather data by a city name.
+    Sample data: https://samples.openweathermap.org/data/2.5/weather?q=London&appid=439d4b804bc8187953eb36d2a8c26a02
+    """
+    url = 'http://api.openweathermap.org/data/2.5/weather'
+    response = requests.get(url, params={'q': city, 'appid': secrets.OPEN_WEATHER_API_KEY}).json()
+
+    if response['cod'] == 429:
+        # API limit has been reached
+        raise QuotaError(response['message'])
+
+    return {
+        'city': city,
+        'temp': response['main']['temp'],
+        'description': response['weather'][0]['description'],
+        'icon': response['weather'][0]['icon'],
+    }
+
+
+if __name__ == '__main__':
+    from pprint import pprint
+    pprint(get_weather_data('Kazan'))
