6f14eda1073d236cc8e1d842be6f001e2b54d30c
Aidar Garikhanov <a1d4r@yandex.ru>
2020-09-11 13:54:34 +0300

    Add delete city link

--- weather/views.py
+++ weather/views.py
@@ -9,12 +9,12 @@ from .forms import CityForm
 def index(request):
     # weather for cities
     cities = City.objects.all()
-    weather_list = [get_weather_data(city.name) for city in cities]
+    cities = [{**get_weather_data(city.name), 'id': city.id} for city in cities]
 
     # form for adding a city
     form = CityForm()
 
-    context = {'weather_list': weather_list, 'form': form}
+    context = {'cities': cities, 'form': form}
     return render(request, 'weather/index.html', context=context)
 
 
@@ -25,3 +25,8 @@ def new_city(request):
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse('index'))
+
+
+def delete_city(request, city_id):
+    City.objects.filter(id=city_id).delete()
+    return HttpResponseRedirect(reverse('index'))
